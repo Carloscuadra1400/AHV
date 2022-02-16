ee244c90fb4e9164fc7160e72c153a0ed17d2c82
Carlos Cuadra <cuadracarlos693@gmail.com>
2022-02-16 10:56:43 -0600

    Primer commit pagina web

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const typed = new Typed('.typ', {
+    strings: [
+        '<i class= "texto">Muebles</i>',
+        '<i class= "texto">Dormitorios</i>',
+        '<i class= "texto">Gaveteros</i>'
+    ],
+    stringElement: '.portada',
+    typeSpeed: 75,
+    startDelay: 500,
+    backSpeed: 50,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1000,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html'
+})
+
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class= "texto">Indumuebles</i>',
+    ],
+    stringElement: '.titl',
+    typeSpeed: 75,
+    startDelay: 500,
+    backSpeed: 50,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1000,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html'
+})
